d90d0fc945b5a6bd1ee661bf5daca6c40804950f
Grzegorz <grzegorz.lulewicz@interia.pl>
2018-11-15 19:18:16 +0100

    Fix for Android

--- src/main/java/com/github/lulewiczg/contentserver/web/servlets/LoginServlet.java
+++ src/main/java/com/github/lulewiczg/contentserver/web/servlets/LoginServlet.java
@@ -2,6 +2,7 @@ package com.github.lulewiczg.contentserver.web.servlets;
 
 import java.io.IOException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +22,15 @@ import com.github.lulewiczg.contentserver.utils.models.UserPermissions;
 public class LoginServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
+    private ServletContext context;
+
+    /**
+     * @see javax.servlet.GenericServlet#init()
+     */
+    @Override
+    public void init() throws ServletException {
+        context = getServletContext();
+    }
 
     /**
      * Returns currently logged user.
@@ -34,7 +44,7 @@ public class LoginServlet extends HttpServlet {
         String user = (String) session.getAttribute(Constants.Web.USER);
         boolean upload = false;
         if (path != null) {
-            upload = ResourceUtil.get(req.getServletContext()).hasWriteAccess(path, user);
+            upload = ResourceUtil.get(context).hasWriteAccess(path, user);
         }
         resp.setContentType(Constants.Setting.APPLICATION_JSON);
         resp.getWriter().write(new UserPermissions(user, upload).toJSON());
